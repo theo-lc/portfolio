2dcf62b9c1340e977808ed6188362587ffd59353
tlecolleter <lecolleter.theo@gmail.com>
2024-02-05 14:43:15 +0100

    Refactor style into different files

---
+++ src/languages/fr.js
@@ -0,0 +1,80 @@
+var french = {
+  /* Sidebar */
+  about: "à propos",
+  skills: "Compétences",
+  experience: "Expérience",
+  projects: "Projets",
+  education: "éducation",
+  backend_developer: "Développeur backend",
+
+  /* About */
+  description:
+    "Je suis un développeur backend avec des compétences en optimisation et en frontend.",
+  strengths: "Qualités",
+  curious: "Curieux",
+  productive: "Productif",
+  learning: "Passion de l'apprentissage",
+  teamwork: "Travail d'équipe",
+  kindness: "Bienveillance",
+  growth_mindset: "Mentalité de croissance",
+
+  /* Skills */
+  optimization: "Optimisation",
+
+  /* Experience */
+  artelys_dates: " Juin 2022 - Décembre 2023 ",
+  ratp: "Maintenance et développement d'une application web pour la maintenance du matériel roulant de la RATP.",
+  agile: "Méthode agile",
+  producing:
+    "Production de code propre et efficient basé sur les spécifications.",
+  testing: "Tests, suivi et résolution d'anomalies",
+  documenting: "Création d'une documentation technique",
+  data: "Analyse de données pour le développement d'un prototype",
+  cleaning: "Nettoyage des données",
+  analyzing: "Analyse des données",
+  interpreting: "Interprétation des données",
+  internship: "Stage de fin  d'études",
+  internship_dates: "Février 2021 - Juin 2021",
+  research:
+    "Projet de recherche sur le thème du dernier-kilomètre en milieu urbain",
+  proposition:
+    "Proposition d'une métaheuristique pour aborder la problématique de densité urbaine et réduire les émissions",
+
+  /* Projects */
+  research_project: "Projet de recherche en transport et logistique",
+  research_project_dates: "Printemps - 2021",
+  internship_description:
+    "J'ai travaillé en tant que stagiaire à l'IMT Atlantique de Nantes sur un projet de recherche sur le sujet du dernier kilomètre.",
+  literature: "Revue de la litérature",
+  modeling: "Modélisation mathématique en Python avec PuLP",
+  implementation:
+    "Implémentation d'une heuristique ALNS (Adaptative Large Neighborhood Search) en Julia",
+  visualization: "Visualisation de carte avec Folium",
+  openrouteservice:
+    "Utilisation de données réalistes obtenues avec OpenRouteService",
+  overleaf:
+    "Rédaction collaborative d'un papier dans le journal EJOR (European Journal of Operational Research)",
+  vrptwd: "Problème de tournées de véhicules avec fenêtres de temps et drones",
+  vrptwd_dates: "Hiver - 2020",
+  vrptwd_description: "",
+  tspd_dates: "Spring",
+
+  /* Education */
+  master: "Master, Université de Nantes",
+  master_dates: "Septembre 2019 - Juin 2021",
+  master_description:
+    "Informatique, parcours Optimisation en Recherche Opérationnelle",
+  discrete: "Optimisation discrète et combinatoire",
+  metaheuristic: "Métaheuristiques",
+  constraint: "Programmation par Contraintes",
+  moo: "Optimisation multiobjectif",
+  mining: "Fouille de données",
+  application: "Cours spécifique au domaine d'application",
+  transportation: "Transport et logistique",
+  scheduling: "Plannification et ordonnancement",
+  bachelor: "Licence, Université de Nantes",
+  bachelor_dates: "Septembre 2016 - Juin 2019",
+  bachelor_description: "Informatique",
+  moncton:
+    "3ème année de licence en échange universitaire à l'Université de Moncton, Nouveau-Brunswick, Canada",
+};
